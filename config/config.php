8208fb03dbe55ccd13e2e2d05f10ff119ca7329f
Leydy Vanesa Riveros Ramirez <113951722+Levarira@users.noreply.github.com>
2023-04-01 22:07:23 -0500

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+    $mysqli = new mysqli("localhost", "root", "", "contactanos");
+    if ($mysqli->connect_errno) {
+        echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
+
+    $mysqli = new mysqli("127.0.0.1", "root", "", "contactanos", 3306);
+    if ($mysqli->connect_errno) {
+        echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
+?>
