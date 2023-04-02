32e02759fbdf69a4754f4719556e5bc2498327b6
Leydy Vanesa Riveros Ramirez <113951722+Levarira@users.noreply.github.com>
2023-04-01 22:00:12 -0500

    Add files via upload

---
+++ templates/dependencias.php
@@ -0,0 +1,22 @@
+<!-- fuente dd -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap" rel="stylesheet">
+<!-- fuente letra de parrafo  -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
+<!-- fuente titulo  -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Merienda:wght@800&display=swap" rel="stylesheet">
+<!-- bootstrap  -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.2/dist/umd/popper.min.js" integrity="sha384-q9CRHqZndzlxGLOj+xrdLDJa9ittGte1NksRmgJKeCV9DrM7Kz868XYqsKWPpAmn" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+<!-- estilos  -->
+<link rel="stylesheet" href="css/style.css">
+<!-- iconos  -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
---
+++ templates/header.php
@@ -0,0 +1,28 @@
+<header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+    <div>
+        <nav class="menu">
+            <a href="index.php" class="logo">
+                <h1 style="text-decoration: none;">EQUILIBRIO <span>Y PERTENENCIA</span></h1>
+            </a>
+<!-- se creo en php
+            <ul class="nav">
+                <li>
+                    <a href="index.php" class="active">Inicio</a>
+                </li>
+                <li>
+                    <a href="comunicacionefectiva.php">Comunicación efectiva</a>
+                </li>
+                <li>
+                    <a href="#Empatía">Empatía</a>
+                </li>
+                <li>
+                    <a href="#Asertividad">Asertividad</a>
+                </li>
+                <li>
+                    <a href="contacto.php">Contacto</a>
+                </li>
+                
+            </ul> -->        
+        </nav>
+    </div>
+</header>
