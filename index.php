731b5b640740bc25224a2ea6cd80bf0ee56cd0a9
Levarira <leidyramirez97@hotmail.com>
2023-04-10 20:13:57 -0500

    Co-authored-by: YURY HASBLEIDY ORJUELA ARANGO <yorjuel5@ibero.edu.co>

--- index.php
+++ index.php
@@ -29,86 +29,116 @@
         include 'templates/header.php';
     ?>
 
-    <img src="Imagenes/10.jpg" alt="">
-    <div class="text">
-    <dd>¿Qué son las habilidades sociales?</dd>
-    <br>
-    <br>
-    <div>
-        <p>
-        Las habilidades sociales (HHSS) para la psicología son un conjunto de conductas y hábitos observables, que fomentan la comunicación eficaz, como, las relaciones satisfactorias entre las personas. En otras palabras, son rasgos de comportamiento y comunicación efectiva, empatía hacia los demás, trabajar en equipo, resolución de conflictos de manera constructiva y así llegar a consolidar relaciones interpersonales positivas
-        </p>
-    </div>
-    <p>
-        Las habilidades sociales son los patrones de compartamiento que adaptamos para lograr objetivos especificos en el diario vivir y como actuamos en cada uno de estos eventos. Por ejemplo: nos permite visualizarnos ante el mundo exterior y como queremos ser vistos, plantear objetivos y poner limites 
-    </P>
-    <dd>Habilidad Social</dd>
-    <br>
-    <br>
-    <p>
-        La habilidad social también implica tener un alto grado de conciencia social, lo que significa que una persona puede reconocer y adaptarse a las normas y expectativas sociales en diferentes contextos. Las personas con habilidades sociales desarrolladas pueden establecer y mantener relaciones saludables, lo que puede tener un impacto positivo en su vida personal y profesional. 
-    </p>
-    <div class="img1"><img class="img2" src="Imagenes/12.jpg" alt=""></div>
-    <dd> Existen varias maneras de mejorar tus habilidades sociales, aquí te presentamos algunas: </dd> 
-    <br>
-    <br>
-
-<div class="container">
-<div class="row">
-    <div class="col-lg-3 col -md-6 col -sm-12"> Practicar la empatía: Como con cualquier habilidad, la práctica es fundamental. Sal y socializa tanto como puedas.
-            Puedes comenzar practicando pequeñas interacciones con personas que conoces, como tus amigos, familiares, compañeros de trabajo o vecinos.</div>
-
-            <div class="col-lg-3 col -md-6 col -sm-12"> Escuchar activamente: Escuchar con atención y mostrando interés en lo que la otra persona está diciendo es una habilidad importante en la comunicación. Escucha lo que la otra persona está diciendo y haz preguntas para profundizar en su perspectiva.</div>
-           
-            <div class="col -lg-3 col -md-6 col -sm-12"> Ser amable y empático: Trata a los demás con respeto, amabilidad y empatía. Intenta ponerte en su lugar y comprender sus sentimientos y perspectivas.</div>
-            
-           
-</div>
-</div>
-
-<div class="container">
-    <div class="row">
-    <div class="col -lg-3 col -md-6 col -sm-12">Mejorar tu lenguaje corporal: La forma en que te mueves, miras y te sientas puede afectar la forma en que te perciben los demás. Asegúrate de tener un lenguaje corporal positivo, mira a los ojos cuando hablas con alguien, sonríe y mantén una postura abierta.</div>
-            
-            <div class="col-lg-3 col -md-6 col -sm-12">Trabajar en la comunicación no verbal: La comunicación no verbal incluye la entonación, el tono de voz y los gestos que utilizas. Intenta practicar la comunicación no verbal para transmitir tus emociones y pensamientos de manera más efectiva.</div>
-           
-            <div class="col-lg-3 col -md-6 col -sm-12"> Aprender a manejar conflictos: Los conflictos son inevitables en la vida, pero es importante saber cómo manejarlos de manera efectiva. Practica la resolución de conflictos escuchando a la otra persona, tratando de encontrar una solución peligrosa beneficiosa y manteniendo la calma en situaciones tensas.</div>
-        
-           <div class="col-lg-3 col -md-6 col -sm-12"> Buscar retroalimentación: Pídele a amigos o familiares que te den retroalimentación sobre tus habilidades sociales. Pregúntales en qué áreas necesitas mejorar y utiliza esa información para trabajar en ti mismo.</div>    
-    </div>
-</div>
-    <dd>Algunas habilidades sociales importantes incluyen:</dd>
-    <br>
-    <br>
-    <dd>Comunicación efectiva</dd>
-    <dt class="definicion">
-    La comunicación efectiva es la compresión de las emociones y a su vez las intenciones que tiene la información y para lograr esto claramente se debe empezar con saber escuchar así el significado sea entendido y así logramos a su vez que la otra persona se sienta escuchada y entendida.
-    </dt>
-    <br>
-    <dd>Empatía </dd>
-    <dt class="definicion">
-        Ser capaz de entender y sentir las emociones de los demás
-    </dt>
-    <br>
-    <dd>Asertividad </dd> 
-    <dt class="definicion">
-    La asertividad es una forma de comunicación que consiste en defender tus derechos, expresar tus opiniones y realizar sugerencias de forma honesta, sin caer en la agresividad o la pasividad, respetando a los demás pero sobretodo respetando tus propias necesidades.
-    </dt>
-    <br>
-    <dd> Habilidad para resolver conflictos</dd>
-    <dt class="definicion">
+    <img src="Imagenes/header2.png" alt="">
     
-    </dt>
-    <br>
-    <dd>Habilidad para trabajar en equipo</dd>
-    <dt class="definicion">
-        Ser capaz de trabajar de manera efectiva con otros y colaborar 
+     <section class="text">
 
+        <div class="text1">
+        <p class="titulo1">¿Qué son las habilidades sociales?</p>
+        <p class="parr1">
+            Las habilidades sociales (HHSS) para la psicología, es un conjunto de conductas y habitos observables, que
+            fomentan la comunicación eficaz, como las relaciones satisfactorias entre las personas basado en respeto. En
+            otras palabras, son ragos de comportamiento y comunicación que nos hacen lograr comunicarnos de manera clara y
+            efectiva, escuchar y comprender a los demás, mostrar empatía y comprensión, trabajar en equipo, resolver
+            conflictos de manera constructiva y consolidar relaciones interpersonales positivas.
+        </p>
+        </div>
+        <div class="imagens2">
+        <img src="Imagenes/seccion2.png" alt="">
+        </div>
+        </section>
 
-    </dt> 
-    </div>
+        <dd> Hay varias maneras de mejorar tus habilidades sociales, aquí te presentamos algunas: </dd>
+        <br>
+        <br>
+        <section class="divicion">
+
+        <div class="grid">
+        <div class="textos-hero">
+            <img src="Imagenes/imag1.svg" class="ico">
+            <div class="item">
+            <b>Practicar la empatía</b>
+            </div>
+        </div>
+        </div>
+        <div class="grid">
+            <div class="textos-hero">
+            <img src="Imagenes/imag2.svg" class="ico">
+            <div class="item">
+                <b>Escuchar activamente</b>
+            </div>
+            </div>
+        </div>
+        <div class="grid">
+            <div class="textos-hero">
+                <img src="Imagenes/imag3.svg" class="ico">
+                <div class="item">
+                <b>Ser amable y empático</b>
+                </div>
+            </div>
+            </div>
+        </section>
+        <section class="divicion2">
+        <div class="grid">
+        <div class="textos-hero">
+            <img src="Imagenes/imag4.svg" class="ico">
+            <div class="item">
+            <b>Mejorar tu lenguaje corporal</b>
+            </div>
+        </div>
+        </div>
+        <div class="grid">
+            <div class="textos-hero">
+            <img src="Imagenes/imag5.svg" class="ico">
+            <div class="item">
+                <b>Trabajar en la comunicación no verbal</b>
+            </div>
+            </div>
+        </div>
+        <div class="grid">
+            <div class="textos-hero">
+                <img src="Imagenes/imag6.svg" class="ico">
+                <div class="item">
+                <b>Aprender a manejar conflictos</b>
+                </div>
+            </div>
+            </div>
+        <div class="grid">
+            <div class="textos-hero">
+                <img src="Imagenes/imag7.svg" class="ico">
+                <div class="item">
+                    <b>Buscar retroalimentación</b>
+                </div>
+                </div>
+            </div>
+     </section>
+    
 
     <script src="js/menu.js"></script>
     
 </body>
+
+<footer>
+
+  <div class="contenedor-footer">
+    <div class="content-foo">
+      <h2>Telefono</h2>
+      <p class="blancas">3186305691</p>
+    </div>
+    
+    <div class="content-foo">
+      <h2>Correo</h2>
+      <p class="blancas">Equilibrio.perte@gmail.com</p>
+    </div>
+
+    <div class="content-foo">
+      <h2>Direccion</h2>
+      <p class="blancas">Cl 56 # 90b 29</p>
+    </div>
+  </div>
+
+  <h3 class="titulo-final">&copy; Hasbleidy O | Leydy | Julian | Carol</h3>
+
+</footer>
+
 </html>
