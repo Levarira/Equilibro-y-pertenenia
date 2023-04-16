5e99964cffb6f2a11b644ae243147b6778c2ed5b
Levarira <leidyramirez97@hotmail.com>
2023-04-16 16:38:56 -0500

    actualizaciones

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
 
-    <title>Habilidades Sociales</title>
+    <title>Equilibrio y Pertenencia</title>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,7 +13,6 @@
 	<link href="https://res.cloudinary.com/dxfq3iotg/raw/upload/v1581152092/smartwizard/smart_wizard.min.css" rel="stylesheet" type="text/css" />
 	<link href="https://res.cloudinary.com/dxfq3iotg/raw/upload/v1581152091/smartwizard/smart_wizard_theme_arrows.min.css" rel="stylesheet" type="text/css" />
 	<script type="text/javascript" src="https://res.cloudinary.com/dxfq3iotg/raw/upload/v1581152197/smartwizard/jquery.smartWizard.min.js"></script>
-	<link rel="stylesheet" href="archivos\css\general.css" />
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
@@ -48,151 +47,124 @@
 
 	</div>
 	
-  
-  <div class="card">
-    <div class="row ">
+	<section class="text">
 
-	<div class="col-md-7 px-3">
-        <div class="card-block px-6">
-          <h4 class="titulo1" >Que son las habilidades sociales</h4>
-          
-          <p class="parr1" >
-		  Las habilidades sociales (HHSS) para la psicología, es un conjunto de conductas y habitos observables, que fomentan la comunicación eficaz, como las relaciones satisfactorias entre las personas basado en respeto. En otras palabras, son ragos de comportamiento y comunicación que nos hacen lograr comunicarnos de manera clara y efectiva, escuchar y comprender a los demás, mostrar empatía y comprensión, trabajar en equipo, resolver conflictos de manera constructiva y consolidar relaciones interpersonales positivas
-		
-        </div>
-      </div>
-      
-      <div class="col-md-5">
-        
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img class="d-block" src="imagenes/seccion2.png">
-            </div>
-           
-  
-          </div>
-        </div>
-      </div>
-      
-	</div>
-  </div>
-  
+<div class="text1">
+	<p class="titulo1">¿Qué son las habilidades sociales?</p>
+	<p class="parr1">
+		Las habilidades sociales (HHSS) para la psicología, es un conjunto de conductas y habitos observables, que
+		fomentan la comunicación eficaz, como las relaciones satisfactorias entre las personas basado en respeto. En
+		otras palabras, son ragos de comportamiento y comunicación que nos hacen lograr comunicarnos de manera clara y
+		efectiva, escuchar y comprender a los demás, mostrar empatía y comprensión, trabajar en equipo, resolver
+		conflictos de manera constructiva y consolidar relaciones interpersonales positivas.
+	</p>
+</div>
+<div class="imagens2">
+<img src="Imagenes/seccion2.png" alt="">
+</div>
+</section>
 
-
- 
-      </div>
-    </div>
-  </div>
- 
- <br>
+<dd> Hay varias maneras de mejorar tus habilidades sociales, aquí te presentamos algunas: </dd>
+<br>
 <br>
 
 
-			
-		<div class="container">
-
-			<span>
-				<h3 style="text-align: center; font-family: cursive;">
-				¿Cómo puedes mejorar tus habilidades sociales?</h3>
-			</span>
-
-			<div class="row pb-5 mb-4">
-				<div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-					
-					<div class="card rounded shadow-sm border-0">
-						<div class="card-body p-4"><img src="imagenes/imag1.svg" alt="" class="img-fluid d-block mx-auto mb-3">
-							<h5> <a class="text-dark" style="font-family: cursive;">Practicar La Empatia</a>
-						<li>
-							Respetando los puntos de opinión.
-						 </li>
-					<li>
-						Entrenar la capacidad de entender las propias emociones para entender la de los demás.
-					</li>
-				<li>
-					Evita aconsejar si no hay una petición expresa.
-				</li>
-			<li></li></h5>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-					
-					<div class="card rounded shadow-sm border-0">
-						<div class="card-body p-4"><img src="imagenes/imag2.svg" alt="" class="img-fluid d-block mx-auto mb-3">
-							<h5> <a class="text-dark" style="font-family: cursive;">Escuchar Activamente</a>
-						<li>
-							Escucha sin emitir juicios.
-						</li>
-					<li>
-						Generar confianza y establecer una buena relación.
-					</li>
-				<li>
-					Haz preguntas abiertas y especificas
-				</li></h5>
-
-						</div>
-					</div>
-				</div>
-
-
-				<div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-					
-					<div class="card rounded shadow-sm border-0">
-						<div class="card-body p-4"><img src="imagenes/imag3.svg" alt="" class="img-fluid d-block mx-auto mb-3">
-							<h5> <a class="text-dark" style="font-family: cursive;">Ser Amable y Empatico</a></h5>
-
-						</div>
-					</div>
-				</div>
-
-				<div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-					
-					<div class="card rounded shadow-sm border-0">
-						<div class="card-body p-4"><img src="imagenes/imag4.svg" alt="" class="img-fluid d-block mx-auto mb-3">
-							<h5> <a class="text-dark" style="font-family: cursive;">Mejorar Tu Lenguaje Corporal</a></h5>
-
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-					
-					<div class="card rounded shadow-sm border-0">
-						<div class="card-body p-4"><img src="imagenes/imag5.svg" alt="" class="img-fluid d-block mx-auto mb-3">
-							<h5> <a class="text-dark" style="font-family: cursive;">Trabajar en la Comunicacion No verbal</a></h5>
-
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-					
-					<div class="card rounded shadow-sm border-0">
-						<div class="card-body p-4"><img src="imagenes/imag6.svg" alt="" class="img-fluid d-block mx-auto mb-3">
-							<h5> <a class="text-dark" style="font-family: cursive;">Aprender a manejar conflictos</a></h5>
-
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-					
-					<div class="card rounded shadow-sm border-0">
-						<div class="card-body p-4"><img src="imagenes/imag7.svg"
-						 alt="" class="img-fluid d-block mx-auto mb-3">
-							<h5>
-								
-							<a class="text-dark" style="font-family: cursive;">Buscar Retroalimentacion</a></h5>
-
-						</div>
-					</div>
-				</div>
-			</div>
-			</br>
+<section class="divicion">
+	
+<div class="grid">
+  <div class="textos-hero">
+	<img src="Imagenes/imag1.svg" class="ico">
+	<div class="item">
+	  <b>Practicar la empatía</b>
+	</div>
+  </div>
+</div>
+<div class="grid">
+	<div class="textos-hero">
+	  <img src="Imagenes/imag2.svg" class="ico">
+	  <div class="item">
+		<b>Escuchar activamente</b>
+	  </div>
+	</div>
+</div>
+<div class="grid">
+	  <div class="textos-hero">
+		<img src="Imagenes/imag3.svg" class="ico">
+		<div class="item">
+		  <b>Ser amable y empático</b>
 		</div>
+	  </div>
+	</div>
+</section>
+<section class="divicion2">
+<div class="grid">
+  <div class="textos-hero">
+	<img src="Imagenes/imag4.svg" class="ico">
+	<div class="item">
+	  <b>Mejorar tu lenguaje corporal</b>
+	</div>
+  </div>
+</div>
+<div class="grid">
+	<div class="textos-hero">
+	  <img src="Imagenes/imag5.svg" class="ico">
+	  <div class="item">
+		<b>Trabajar en la comunicación no verbal</b>
+	  </div>
+	</div>
+  </div>
+<div class="grid">
+	<div class="textos-hero">
+		<img src="Imagenes/imag6.svg" class="ico">
+		<div class="item">
+		  <b>Aprender a manejar conflictos</b>
 		</div>
+	  </div>
+	</div>
+<div class="grid">
+	<div class="textos-hero">
+		  <img src="Imagenes/imag7.svg" class="ico">
+		  <div class="item">
+			<b>Buscar retroalimentación</b>
+		  </div>
+		</div>
+	  </div>
+</section>
+
+
+
 </body>
 
 
-<?php include 'templates/footer.php'; ?>
+<footer>
+
+<div class="contenedor-footer">
+<div class="content-foo">
+  <h2>Telefono</h2>
+  <p class="blancas">3186305691</p>
+</div>
+
+<div class="content-foo">
+  <h2>Correo</h2>
+  <p class="blancas">Equilibrio.perte@gmail.com</p>
+</div>
+
+<div class="content-foo">
+  <h2>Direccion</h2>
+  <p class="blancas">Cl 56 # 90b 29</p>
+</div>
+</div>
+
+<h3 class="titulo-final">&copy; Hasbleidy O | Leydy | Julian | Carol</h3>
+
+</footer>
+
+
+  
+
+</body>
+
+
 
 
 </html>
--- templates/footer.php
+++ templates/footer.php
@@ -1,52 +1,22 @@
-<footer class="footer-distributed">
+<footer>
+
+  <div class="contenedor-footer">
+    <div class="content-foo">
+      <h2>Telefono</h2>
+      <p class="blancas">3186305691</p>
+    </div>
+    
+    <div class="content-foo">
+      <h2>Correo</h2>
+      <p class="blancas">Equilibrio.perte@gmail.com</p>
+    </div>
+
+    <div class="content-foo">
+      <h2>Direccion</h2>
+      <p class="blancas">Cl 56 # 90b 29</p>
+    </div>
+  </div>
  
-	<div class="footer-left">
-
-	 <h3>Equilibrio y pertenencia
-		<br>
-		<br>
-		<br>		
-	
-		<p class="footer-company-name" style="color: white; font-family: cursive;">Realizado por:&copy; Hasbleidy O | Leydy | Julian | Carol</p>
-	</div>
-
-	<div class="footer-center">
-
-		<div>
-			<i class="fa fa-map-marker"></i>
-			<p>Bogota, Cundinamarca, Colombia</p>
-		</div>
-
-		<div>
-			<i class="fa fa-phone"></i>
-			<p>3186305691</p>
-		</div>
-
-		<div>
-			<i class="fa fa-envelope"></i>
-			<p>Equilibrio.perte@gmail.com</p>
-		</div>
-
-	</div>
-
-	<div class="footer-right">
-
-		<p style="color: white;" class="footer-company-about">
-			<span>Quienes somos</span>
-			Somos un grupo de Psicologos interesandos en tu salud mental, por ende si deseas acceder a información o requieres apoyo psicológico, ponemos a tus servicios diferentes tipos de accesorias con la finalidad de ayudarte. 
-          ¡Queremos ser tu mayor aliado en la búsqueda de tú salud y el bienestar emocional!
-
-		</p>
-
-	</div>
-	<div class="footer-icons">
-
-		<a href="#"><i class="fa fa-facebook"></i></a>
-		<a href="#"><i class="fa fa-twitter"></i></a>
-		<a href="#"><i class="fa fa-instagram"></i></a>
-
-
-	</div>
-
+  class="titulo-final">&copy; Hasbleidy O | Leydy | Julian | Carol</h3>
 
 </footer>
