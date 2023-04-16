c7c2b64b0ebc4375be57ca5eed148cc129b77d2a
Levarira <leidyramirez97@hotmail.com>
2023-04-16 13:48:26 -0500

    Co-authored-by: YURY HASBLEIDY ORJUELA ARANGO <yorjuel5@ibero.edu.co>

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 
-	<title>PSICOLOGIA</title>
+    <title>Habilidades Sociales</title>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,17 +20,13 @@
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-	<!--Floating WhatsApp css-->
+
 	<link rel="stylesheet" href="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/floating-wpp.min.css">
-	<!--Floating WhatsApp javascript-->
+
 	<script type="text/javascript" src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/floating-wpp.min.js"></script>
 
 
 	<?php include 'templates/header.php'; ?>
-
-
- <title>Equilibrio y Pertenencia</title>
-
 
 <br>
 <br>
@@ -58,16 +53,16 @@
 	</br>
 	<div class="container">
 	<div class="container py-3">
-  <!-- Card Start -->
+  
   <div class="card">
     <div class="row ">
 
       <div class="col-md-7 px-3">
         <div class="card-block px-6">
-          <h4 class="card-title" style="font-family: 'Courier New', Courier, monospace; color:black;">Que son las habilidades sociales</h4>
+          <h4 class="card-title" style="font-family: 'Courier New', Courier, monospace; color:black;">¿Qué son las habilidades sociales?</h4>
           
           <p class="card-text" style="font-family: 'Courier New', Courier, monospace; color:black; ">
-		  Las habilidades sociales (HHSS) para la psicología, es un conjunto de conductas y habitos observables, que fomentan la comunicación eficaz, como las relaciones satisfactorias entre las personas basado en respeto. En otras palabras, son ragos de comportamiento y comunicación que nos hacen lograr comunicarnos de manera clara y efectiva, escuchar y comprender a los demás, mostrar empatía y comprensión, trabajar en equipo, resolver conflictos de manera constructiva y consolidar relaciones interpersonales positivas
+		  Las habilidades sociales (HHSS) para la psicología, son un conjunto de conductas y hábitos observables, que fomentan la comunicación eficaz, como las relaciones satisfactorias. En otras palabras, son rasgos de comportamiento y comunicación que nos llevan a tener una comunicación de manera clara y efectiva, escuchar y comprender a los demás, trabajar en equipo, resolver conflictos de manera constructiva y consolidar relaciones interpersonales positivas.
 		  </br> 
 	
 		</br> 
@@ -77,7 +72,7 @@
          
         </div>
       </div>
-      <!-- Carousel start -->
+      
       <div class="col-md-5">
         
           <div class="carousel-inner">
@@ -89,10 +84,10 @@
           </div>
         </div>
       </div>
-      <!-- End of carousel -->
-    </div>
+      
+	</div>
   </div>
-  <!-- End of card -->
+  
 
 
  
@@ -108,24 +103,37 @@
 		<div class="container">
 
 			<span>
-				<h3 style="text-align: center; font-family: cursive;">Como Mejorar Tus Habilidades Sociales</h3>
+				<h3 style="text-align: center; font-family: cursive;">¿Cómo puedes mejorar tus habilidades sociales?</h3>
 			</span>
 
 			<div class="row pb-5 mb-4">
 				<div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-					<!-- Card-->
+					
 					<div class="card rounded shadow-sm border-0">
 						<div class="card-body p-4"><img src="https://www.dropbox.com/s/d1ec8lrd5hspdxn/imag1.svg?raw=1" alt="" class="img-fluid d-block mx-auto mb-3">
-							<h5> <a class="text-dark" style="font-family: cursive;">Practicar La Empatia</a></h5>
+							<h5> <a class="text-dark" style="font-family: cursive;">Practicar La Empatia</a>
+						<li>Respetando los puntos de opinión. </li>
+					<li>Entrenar la capacidad de entender las propias emociones para entender la de los demás.</li>
+				<li>Evita aconsejar si no hay una petición expresa.</li>
+			<li></li></h5>
 						</div>
 					</div>
 				</div>
 
 				<div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-					<!-- Card-->
+					
 					<div class="card rounded shadow-sm border-0">
 						<div class="card-body p-4"><img src="https://www.dropbox.com/s/q4u6z21bmxhar1e/imag2.svg?raw=1" alt="" class="img-fluid d-block mx-auto mb-3">
-							<h5> <a class="text-dark" style="font-family: cursive;">Escuchar Activamente</a></h5>
+							<h5> <a class="text-dark" style="font-family: cursive;">Escuchar Activamente</a>
+						<li>
+							Escucha sin emitir juicios.
+						</li>
+					<li>
+						Generar confianza y establecer una buena relación.
+					</li>
+				<li>
+					Haz preguntas abiertas y especificas
+				</li></h5>
 
 						</div>
 					</div>
@@ -133,7 +141,7 @@
 
 
 				<div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-					<!-- Card-->
+					
 					<div class="card rounded shadow-sm border-0">
 						<div class="card-body p-4"><img src="https://www.dropbox.com/s/6zmggp02i2rdiz7/imag3.svg?raw=1" alt="" class="img-fluid d-block mx-auto mb-3">
 							<h5> <a class="text-dark" style="font-family: cursive;">Ser Amable y Empatico</a></h5>
@@ -143,7 +151,7 @@
 				</div>
 
 				<div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-					<!-- Card-->
+					
 					<div class="card rounded shadow-sm border-0">
 						<div class="card-body p-4"><img src="https://www.dropbox.com/s/2aoniscm2ytlv4d/imag4.svg?raw=1" alt="" class="img-fluid d-block mx-auto mb-3">
 							<h5> <a class="text-dark" style="font-family: cursive;">Mejorar Tu Lenguaje Corporal</a></h5>
@@ -152,7 +160,7 @@
 					</div>
 				</div>
 				<div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-					<!-- Card-->
+					
 					<div class="card rounded shadow-sm border-0">
 						<div class="card-body p-4"><img src="https://www.dropbox.com/s/bmnzb72etv2ehum/imag5.svg?raw=1" alt="" class="img-fluid d-block mx-auto mb-3">
 							<h5> <a class="text-dark" style="font-family: cursive;">Trabajar en la Comunicacion No verbal</a></h5>
@@ -161,7 +169,7 @@
 					</div>
 				</div>
 				<div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-					<!-- Card-->
+					
 					<div class="card rounded shadow-sm border-0">
 						<div class="card-body p-4"><img src="https://www.dropbox.com/s/y0nkermnxc8r1af/imag6.svg?raw=1" alt="" class="img-fluid d-block mx-auto mb-3">
 							<h5> <a class="text-dark" style="font-family: cursive;">Aprender a manejar conflictos</a></h5>
@@ -170,7 +178,7 @@
 					</div>
 				</div>
 				<div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-					<!-- Card-->
+					
 					<div class="card rounded shadow-sm border-0">
 						<div class="card-body p-4"><img src="https://www.dropbox.com/s/7wewadofvvt9pmo/imag7.svg?raw=1" alt="" class="img-fluid d-block mx-auto mb-3">
 							<h5> <a class="text-dark" style="font-family: cursive;">Buscar Retroalimentacion</a></h5>
